a59453772eb5c256d4ada7a79d26bef889625a33
ay <ay@ay.io>
2025-05-26 15:32:46 +1000

    2025-05-26 (1) Add PhoneNumberValidator to __init__.py and new file
    - 1 M:1
    - 1 file changed, 2 insertions(+)

--- src/validated_llm/validators/__init__.py
+++ src/validated_llm/validators/__init__.py
@@ -5,6 +5,7 @@ Built-in validators for common validation patterns.
 from .date_time import DateTimeValidator
 from .email import EmailValidator
 from .markdown import MarkdownValidator
+from .phone_number import PhoneNumberValidator
 from .url import URLValidator
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "DateTimeValidator",
     "EmailValidator",
     "MarkdownValidator",
+    "PhoneNumberValidator",
 ]
